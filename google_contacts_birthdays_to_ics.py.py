543fc5130f1c21f7fb827b1a45bfa7527cc64b8d
Paul Mathias Nelde <paul@funkhaus.io>
2026-03-12 14:50:22 +0100

    update: handle expired token flow

--- google_contacts_birthdays_to_ics.py.py
+++ google_contacts_birthdays_to_ics.py.py
@@ -3,6 +3,8 @@ import os.path
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+from google.auth.exceptions import RefreshError
 from icalendar import Calendar, Event
 
 # OAuth scope: read-only access to Google Contacts
@@ -21,12 +23,16 @@ def authenticate():
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh()
-        else:
+            try:
+                creds.refresh(Request())
+            except RefreshError:
+                creds = None
+
+        if not creds:
             flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+    with open('token.json', 'w') as token:
+        token.write(creds.to_json())
     return creds
 
 def fetch_birthdays(service):
